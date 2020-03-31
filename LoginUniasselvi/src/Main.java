c926955a44a8583dc0b0e9e92a0e0f2bfaa3b32f
FulllLinux <fulllinux@hotmail.com>
2020-03-30 21:29:35 -0300

    Inclusao das classes bases do app LoginUniasselvi

---
+++ LoginUniasselvi/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		PessoaFisica pessoa1Pessoa = new PessoaFisica();
+		pessoa1Pessoa.setNome( "Everton" );
+		pessoa1Pessoa.setCpf( "014152416461" );
+		/**/
+		System.out.println( pessoa1Pessoa.getNome() );
+	}
+
+}
